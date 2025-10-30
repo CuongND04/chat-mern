1808c7766681fc8b45e936da61d6d7aef52f606e
Bo2874 <ngovanbo280704@gmail.com>
2025-10-30 08:08:01 +0700

    signup, login and logout

--- source/server/src/controllers/auth.controller.js
+++ source/server/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
-import { generateToken } from "../lib/utils.js";
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+// import cloudinary from "../lib/cloudinary.js"
+import { generateToken } from "../lib/utils.js"
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
@@ -48,4 +50,44 @@ export const signup = async (req, res) => {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
-};
+}
+
+export const login = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    // check if user exists
+    const user = await User.findOne({ email })
+    if (!user) {
+      // email not found and wrong pass, put a generic message avoid a malicious user
+      return res.status(400).json({ message: "Invalid credentials" })
+    }
+    // check password 
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" })
+    }
+    // create jwt 
+    generateToken(user._id, res)
+    // return response for login request
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic
+    })
+  } catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(500).json({ message: "Internal Server Error" })
+  }
+}
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 })
+    res.status(200).json({ message: "Logged out successfully" })
+  } catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(500).json({ message: "Internal Server Error" })
+  }
+
+}
